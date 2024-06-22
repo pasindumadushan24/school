5a528bd30d30be52b39b3895918900a19dba8322
Pasindu Madushan <pasindu24hh@gmail.com>
2024-06-22 09:29:04 +0530

    updates

--- src/main/java/lk/ijse/Main.java
+++ src/main/java/lk/ijse/Main.java
@@ -1,8 +1,5 @@
 package lk.ijse;
 
-
-
-
 public class Main {
     public static void main(String[] args) {
         m.main(args);
--- src/main/java/lk/ijse/m.java
+++ src/main/java/lk/ijse/m.java
@@ -1,11 +1,5 @@
 package lk.ijse;
 
-
-
-
-
-
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
